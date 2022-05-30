11f4fd105d4112749894f4aeb21b37ece183f7a9
rmcstt <anisio.nanato@hotmail.com>
2022-05-30 11:49:47 -0300

    pelo pycharm

---
+++ python 1/aula3/aula3.py
@@ -0,0 +1,18 @@
+# tudo em python é objeto
+
+"""
+str - string
+"""
+print('aspas simples')
+print("aspas duplas")
+print(1234)
+
+print(1 + 1)
+print("1" + "1")  # parecido com JS
+
+print("essa é uma 'string' (str).")  # ✅
+print('essa é uma "string" (str).')  # ✅
+print("essa é uma \"string\" (str).")  # ❌
+print('essa é uma \'string\' (str).')  # ❌
+print('essa é uma \nstring\' (str).')  # ❌ \n quebra a linha
+print(r'essa é uma \nstring\' (str).')  # ❌ "r" antes da string pede pra nao executar nada dentro do texto
