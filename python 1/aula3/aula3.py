0da20898917a2960d36957364d739c83081512b9
rmcstt <anisio.nanato@hotmail.com>
2022-05-30 12:41:24 -0300

    pelo pycharm 2

--- python 1/aula3/aula3.py
+++ python 1/aula3/aula3.py
@@ -16,3 +16,16 @@ print("essa é uma \"string\" (str).")  # ❌
 print('essa é uma \'string\' (str).')  # ❌
 print('essa é uma \nstring\' (str).')  # ❌ \n quebra a linha
 print(r'essa é uma \nstring\' (str).')  # ❌ "r" antes da string pede pra nao executar nada dentro do texto
+
+# aspas simples
+# aspas duplas
+# 1234
+# 2
+# 11
+# essa é uma 'string' (str).
+# essa é uma "string" (str).
+# essa é uma "string" (str).
+# essa é uma 'string' (str).
+# essa é uma
+# string' (str).
+# essa é uma \nstring\' (str).
