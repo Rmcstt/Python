1e0cdf39e1101d6b96c631e03d2c1b6b3d2aa659
rmcstt <anisio.nanato@hotmail.com>
2022-07-19 14:20:40 -0300

    py

---
+++ python 1/exercicios/exercicios5.py
@@ -0,0 +1,32 @@
+def olaMundo():
+  return 'ola mundo'
+
+def mestre(funcao):
+  return funcao()
+
+executando = mestre(olaMundo)
+print(executando)
+
+
+def func2():
+  return 'renato'
+
+def func1(param):
+  return param()
+
+printar = func1(func2)  # tem que haver ainda um elo fora das funções para que elas possam ser chamadas.
+print(printar)
+
+def motor():
+  print('vruuummmm') 
+
+def Chave(inginicao):
+  return inginicao()
+
+def giraChave():
+  return Chave(motor)
+
+
+giraChave()
+
+
--- python 1/funçøes (def) /funcoes9.py
+++ python 1/funçøes (def) /funcoes9.py
@@ -1,11 +1,12 @@
 variavel = 'valor'
 
-def func():  
+def func():
   outra_variavel = 'renato'  # variavel local
   return outra_variavel  # retorna o valor de outra_variavel
 
-def func2(arg):  
-  print(arg)  
+def func2(arg):
+  print(arg)   
 
 var = func()
 func2(var)  # imprime 'valor'
+
