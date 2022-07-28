fe849439ab1c58397e81e84802cef301da18d43f
rmcstt <anisio.nanato@hotmail.com>
2022-07-28 10:42:55 -0300

    1

---
+++ python 1/iteraveis/iteraveis.py
@@ -0,0 +1,12 @@
+lista = [1,2,3,4,5]
+print(hasattr(lista, '__iter__'))  # True
+print(hasattr(lista, '__next__'))  # False
+
+lista = iter(lista)  # cria um iterador
+print(hasattr(lista, '__next__'))  # True
+
+print(next(lista))  # 1
+print(next(lista))  # 2
+print(next(lista))  # 3
+print(next(lista))  # 4
+print(next(lista))  # 5
