430981194733bc854ea11898c946d3583038779f
rmcstt <anisio.nanato@hotmail.com>
2022-08-05 16:42:37 -0300

    friday

---
+++ python 1/produtodPy/mapeamento.py
@@ -0,0 +1,34 @@
+from copy import deepcopy
+from produtos import pessoas, produtos, lista  # importação de produtos.py
+
+novaLista = map(lambda x: x * 2, lista)
+# o map ira pegar a lista original com a função lambda e multiplicar por 2, passando a lista como parametro
+novaLista2 = [x * 2 for x in lista]
+# list comprehension - forma mais facil de fazer o map
+print(lista)  # lista original
+print(list(novaLista))  # [2, 4, 6, 8, 10, 12, 14, 16, 18, 20] o map nao converte diretamente para lista, po isso isei o list()
+print(novaLista2)  # [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+########################################################################################################################
+
+def desconto(p):  # função para calcular o desconto
+  p['price'] = round(p['price'] * 0.9,2)  # calcula o desconto e arredonda para 2 casas decimais
+  return p  # retorna o produto com o desconto
+
+novosPrecos = map(desconto, produtos)  # com o map eu chamo a função desconto para cada produto da lista
+soPreco = map(lambda x: x['price'], novosPrecos)  # com esse outro map eu pego apenas os produtos da lista
+
+
+for preco in soPreco:  #... para cada preco da lista de precos...
+  print(preco)  #improme apenas os precos com desconto
+
+########################################################################################################################
+
+soNome = map(lambda n: n['nome'], pessoas)
+
+for nome in soNome:
+  print(f'\n{nome}')
+
+
+
+
--- python 1/produtodPy/produtos.py
+++ python 1/produtodPy/produtos.py
@@ -3,98 +3,106 @@ produtos = [
       "id":1,
       "name":"celular",
       "description":" celular de entrada com 6 cameras",
-      "price":"R$ 999,99",
+      "price":999.90,
       "available": True
    },
    {
       "id":2,
       "name":"notebook",
       "description":"notebook intermediario",
-      "price":"R$ 1.999,99",
+      "price":2999.90,
       "available": True
    },
    {
       "id":3,
       "name":"tablet",
       "description":"tablet 5g",
-      "price":"R$ 899,99",
+      "price":899.90,
       "available": True
    },
    {
       "id":4,
-      "name":"fone bluetooth",
+      "name":"fone",
       "description":"fone bluetooth com cancelamento de ruido",
-      "price":"R$ 99,99",
+      "price":199.90,
       "available": True
    },
    {
       "id":5,
       "name":"mouse",
       "description":"mouse com botao duplo",
-      "price":"R$ 199,99",
+      "price":99.90,
       "available": True
    },
    {
       "id":6,
       "name":"teclado",
       "description":"teclado com iluminação RGB",
-      "price":"R$ 89,99",
+      "price":69.90,
       "available": True
    },
    {
       "id":7,
-      "name":"monitor 4k led",
+      "name":"monitor",
       "description":"monitor com resolução 4k e taxa de atualização de 144hz",
-      "price":"R$ 2.999,99",
+      "price":1999.90,
       "available":True
    },
    {
       "id":8,
       "name":"mousepad",
       "description":"mousepad anti-atraso",
-      "price":"R$ 69,99",
+      "price":49.90,
       "available":True
    },
    {
       "id":9,
-      "name":"hub usb-c",
+      "name":"hub",
       "description":"hub usb-c com entradas; usb, micro-usb, micro-sd, micro-sdx, micro-sdxc",
-      "price":"R$ 299,99",
+      "price":149.90,
       "available":True
    },
    {
       "id":10,
       "name":"headset",
       "description":"headset com microfone e iluminação RGB",
-      "price":"R$ 179,99",
+      "price":299.90,
       "available":True
    },
    {
       "id":11,
-      "name":"HD externo 2tb",
+      "name":"HD",
       "description":"hd SSD com 2tb de capacidade",
-      "price":"R$ 999,99",
+      "price":999.90,
       "available":True
    },
    {
       "id":12,
-      "name":"HD externo 4tb",
-      "description":"hd SSD com 4tb de capacidade",
-      "price":"R$ 1.699,99",
-      "available":True
-   },
-   {
-      "id":13,
-      "name":"cadeira gamer",
+      "name":"cadeira",
       "description":"cadeira gamer com encosto para regiao lombar",
-      "price":"R$ 1.299,99",
+      "price":1299.90,
       "available":True
    },
    {
-      "id":14,
+      "id":134,
       "name":"webcam",
       "description":"webcam com resolução de 1440x1080",
-      "price":"R$ 999,99",
+      "price":599.90,
       "available":True
    },
 ]
+
+pessoas = [
+  {'nome': 'joao','idade': 20 },
+  {'nome': 'carlos','idade': 13 },
+  {'nome': 'roberta','idade': 45 },
+  {'nome': 'ricardo','idade':43  },
+  {'nome': 'julia','idade':  23},
+  {'nome': 'mariele','idade': 45 },
+  {'nome': 'silvio','idade': 76 },
+  {'nome': 'jacke','idade': 21 },
+  {'nome': 'mauricio','idade': 18 },
+  {'nome': 'jade','idade': 22 },
+]
+
+lista = [1,2,3,4,5,6,7,8,9,10]
