333e5509daca7db18bb4cfe9c65def2190064aac
rmcstt <anisio.nanato@hotmail.com>
2022-08-05 12:36:32 -0300

    oi

---
+++ python 1/produtodPy/produtos.py
@@ -0,0 +1,100 @@
+produtos = [
+   {
+      "id":1,
+      "name":"celular",
+      "description":" celular de entrada com 6 cameras",
+      "price":"R$ 999,99",
+      "available": True
+   },
+   {
+      "id":2,
+      "name":"notebook",
+      "description":"notebook intermediario",
+      "price":"R$ 1.999,99",
+      "available": True
+   },
+   {
+      "id":3,
+      "name":"tablet",
+      "description":"tablet 5g",
+      "price":"R$ 899,99",
+      "available": True
+   },
+   {
+      "id":4,
+      "name":"fone bluetooth",
+      "description":"fone bluetooth com cancelamento de ruido",
+      "price":"R$ 99,99",
+      "available": True
+   },
+   {
+      "id":5,
+      "name":"mouse",
+      "description":"mouse com botao duplo",
+      "price":"R$ 199,99",
+      "available": True
+   },
+   {
+      "id":6,
+      "name":"teclado",
+      "description":"teclado com iluminação RGB",
+      "price":"R$ 89,99",
+      "available": True
+   },
+   {
+      "id":7,
+      "name":"monitor 4k led",
+      "description":"monitor com resolução 4k e taxa de atualização de 144hz",
+      "price":"R$ 2.999,99",
+      "available":True
+   },
+   {
+      "id":8,
+      "name":"mousepad",
+      "description":"mousepad anti-atraso",
+      "price":"R$ 69,99",
+      "available":True
+   },
+   {
+      "id":9,
+      "name":"hub usb-c",
+      "description":"hub usb-c com entradas; usb, micro-usb, micro-sd, micro-sdx, micro-sdxc",
+      "price":"R$ 299,99",
+      "available":True
+   },
+   {
+      "id":10,
+      "name":"headset",
+      "description":"headset com microfone e iluminação RGB",
+      "price":"R$ 179,99",
+      "available":True
+   },
+   {
+      "id":11,
+      "name":"HD externo 2tb",
+      "description":"hd SSD com 2tb de capacidade",
+      "price":"R$ 999,99",
+      "available":True
+   },
+   {
+      "id":12,
+      "name":"HD externo 4tb",
+      "description":"hd SSD com 4tb de capacidade",
+      "price":"R$ 1.699,99",
+      "available":True
+   },
+   {
+      "id":13,
+      "name":"cadeira gamer",
+      "description":"cadeira gamer com encosto para regiao lombar",
+      "price":"R$ 1.299,99",
+      "available":True
+   },
+   {
+      "id":14,
+      "name":"webcam",
+      "description":"webcam com resolução de 1440x1080",
+      "price":"R$ 999,99",
+      "available":True
+   },
+]
