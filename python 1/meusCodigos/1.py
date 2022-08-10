aa929682ffea00a2fa68d243e28e59812fb87c1d
rmcstt <anisio.nanato@hotmail.com>
2022-08-10 13:29:32 -0300

    first

--- python 1/meusCodigos/1.py
+++ python 1/meusCodigos/1.py
@@ -1,16 +1,18 @@
 
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import pandas as ListedColormap
-# tabuleiro = np.tile([1,0],(8,4))
-# for i in range(tabuleiro.shape[0]):
-#   tabuleiro[i] = np.roll(tabuleiro[i], i %2)
-# mapa_de_cores = ListedColormap(['#FF0000', '#00FF00'])
-# plt.matshow(tabuleiro, cmap=mapa_de_cores)
-# plt.xticks([])
-# plt.yticks([])
-# plt.show() 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as ListedColormap
+import sys
+tabuleiro = np.tile([1,0],(8,4))
+for i in range(tabuleiro.shape[0]):
+  tabuleiro[i] = np.roll(tabuleiro[i], i %2)
+mapa_de_cores = ListedColormap(['#FF0000', '#00FF00'])
+plt.matshow(tabuleiro, cmap=mapa_de_cores)
+plt.xticks([])
+plt.yticks([])
+plt.show() 
 
+print(sys.path)
 ##############################################################################################
 
 # import pandas as pd
---
+++ python 1/modulosPy/1.py
@@ -0,0 +1,23 @@
+#  Modulos padrão do Python:
+#  Modulos såo arquivos  .py (que contem codigo python) e servem expandir as funcionalidades padråo da linguagem
+#  veja todos os modulos padroes em: https://docs.python.org/3/py-modindex.html
+
+
+from sys import platform as so
+import sys
+from random import randint as randintOri
+import pymysql
+
+
+
+def randint(*args):
+    return 'hahahahahah'
+
+
+print(so)
+for i in range(10):
+    print(randintOri(0, 10), randint())
+
+print(sys.path)
+
+
