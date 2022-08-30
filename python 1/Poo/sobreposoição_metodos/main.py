80a18884676eddcee92bf44aff605935eb5ecc97
rmcstt <anisio.nanato@hotmail.com>
2022-08-30 13:29:00 -0300

    1

---
+++ python 1/Poo/sobreposoição_metodos/main.py
@@ -0,0 +1,13 @@
+from classes import Cliente, Aluno
+
+c1 = Cliente('renato', 26)
+a1 = Aluno('joao', 21)
+
+
+
+
+print(c1.nome, c1.idade)
+c1.falar()
+c1.acao()
+print(a1.nome, a1.idade)
+a1.falar()
