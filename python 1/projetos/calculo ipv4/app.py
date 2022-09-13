1146cba6de3d0cbba04a337718b0ea40fac82e7d
rmcstt <anisio.nanato@hotmail.com>
2022-09-13 09:33:17 -0300

    ip

---
+++ python 1/projetos/calculo ipv4/app.py
@@ -0,0 +1,5 @@
+
+
+from classes.calcipv4 import CalcIpv4
+
+calc_ipv4 = CalcIpv4(ip='192.168.0.1', prefixo=24)
---
+++ python 1/projetos/calculo ipv4/classes/calcipv4.py
@@ -0,0 +1,55 @@
+import re
+
+class CalcIpv4:
+  def __init__(self, ip, mascara=None, prefixo=None):
+    self.ip = ip
+    self.mascara = mascara
+    self.prefixo = prefixo
+
+  @property
+  def ip(self):
+    return self._ip
+  @property
+  def mascara(self):
+    return self._mascara
+  @property
+  def prefixo(self):
+    return self._prefixo
+
+  @ip.setter
+  def ip(self, valor):
+    if not self._valida_ip(valor):
+      raise ValueError('ip invalido')
+    self._ip = valor
+
+  @mascara.setter
+  def mascara(self, valor):
+    if not valor:
+      return
+
+    if not self._valida_ip(valor):
+      raise ValueError('mascara invalda')
+
+  @prefixo.setter
+  def prefixo(self, valor):
+    if not valor:
+      return
+
+    if not isinstance(valor, int):
+      raise TypeError('prefixo deve ser inteiro')
+
+    if valor < 0 or valor > 32:
+      raise ValueError('prefixo deve estar entre 0 e 32') 
+
+  @staticmethod
+  def _valida_ip(ip):
+    """valida o ip informado"""
+    regexp = re.compile(r'^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})$')
+
+    if regexp.search(ip):
+      return True
+
+  
+  @staticmethod
+  def _ip_to_bin(self, ip):
+    """ converte o ip informado para binario """
