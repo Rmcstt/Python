74c82a4cee7ccb2261ac62bb710a686ce82c998b
rmcstt <anisio.nanato@hotmail.com>
2022-09-14 09:47:57 -0300

    quase pronto

--- python 1/projetos/calculo ipv4/app.py
+++ python 1/projetos/calculo ipv4/app.py
@@ -2,4 +2,4 @@
 
 from classes.calcipv4 import CalcIpv4
 
-calc_ipv4 = CalcIpv4(ip='192.168.0.1', prefixo=24)
+calc_ipv4 = CalcIpv4(ip='192.168.0.1', prefixo=24 )
--- python 1/projetos/calculo ipv4/classes/calcipv4.py
+++ python 1/projetos/calculo ipv4/classes/calcipv4.py
@@ -6,6 +6,9 @@ class CalcIpv4:
     self.mascara = mascara
     self.prefixo = prefixo
 
+    self._set_broadcast()
+    self._set_rede()
+
   @property
   def ip(self):
     return self._ip
@@ -22,6 +25,7 @@ class CalcIpv4:
       raise ValueError('ip invalido')
 
     self._ip = valor
+    self._ip_bin = self._ip_to_bin(valor) 
 
   @mascara.setter
   def mascara(self, valor):
@@ -32,7 +36,10 @@ class CalcIpv4:
       raise ValueError('mascara invalda')
 
     self._mascara = valor
-    self._ip_to_bin(valor)
+    self._mascara_bin = self._ip_to_bin(valor)
+
+    if not hasattr(self, '_prefixo'):
+      self._prefixo = self._mascara_bin.count('1')
 
   @prefixo.setter
   def prefixo(self, valor):
@@ -46,6 +53,11 @@ class CalcIpv4:
       raise ValueError('prefixo deve estar entre 0 e 32') 
 
     self._prefixo = valor
+    self._mascara_bin = (valor * '1').ljust(32, '0')
+
+    if not hasattr(self, 'mascara'):
+      self.mascara = self._bin_to_ip(self._mascara_bin)
+    
 
   @staticmethod
   def _valida_ip(ip):
@@ -57,6 +69,32 @@ class CalcIpv4:
 
   
   @staticmethod
-  def _ip_to_bin(self, ip):
+  def _ip_to_bin(ip):
     """ converte o ip informado para binario """
-    print(ip)
+    blocos = ip.split('.')
+    blocos_bin = [x for x in map(lambda x: bin(int(x))[2:].zfill(8),blocos)]
+
+    return ''.join(blocos_bin)
+
+  @staticmethod
+  def _bin_to_ip(ip):
+    """ converte o ip informado para binario"""
+    n = 8
+    blocos = [str(int(ip[i:n+i],2)) for i in range(0, 32, n)]
+    return '.'.join(blocos)
+
+  def _set_broadcast(self):
+    """ calcula o broadcast """
+    host_bits = 32 - self.prefixo
+    self._broadcast_bin = self._ip_bin[:self.prefixo] + (host_bits * '1')
+    self._broadcast = self._bin_to_ip(self._broadcast_bin)
+    return self._broadcast
+
+  def _set_rede(self):
+    """ calcula a rede """
+    host_bits = 32 - self.prefixo
+    self._rede_bin = self._ip_bin[:self.prefixo] + (host_bits * '0')
+    self._rede = self._bin_to_ip(self._rede_bin)
+    return self._rede
+    
+
