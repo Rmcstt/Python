a513ebf8670ec1558f5dcbde0b8cbea4d7ec2f92
rmcstt <anisio.nanato@hotmail.com>
2022-09-15 09:04:54 -0300

    finished

--- python 1/projetos/calculo ipv4/app.py
+++ python 1/projetos/calculo ipv4/app.py
@@ -2,4 +2,6 @@
 
 from classes.calcipv4 import CalcIpv4
 
-calc_ipv4 = CalcIpv4(ip='192.168.0.1', prefixo=24 )
+""" calculo ipv4, com bbase em um ip  e uma mascara ou prefixo"""
+
+calc_ipv4 = CalcIpv4(ip='192.168.0.1' , prefixo=24)
