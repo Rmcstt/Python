4851ec34709e42923148b23701a2d055a4b674ca
rmcstt <anisio.nanato@hotmail.com>
2022-09-16 09:16:38 -0300

    remanejando

--- python 1/projetos/calculo ipv4/app.py
+++ python 1/projetos/calculo ipv4/app.py
@@ -1,7 +1,29 @@
 
 
 from classes.calcipv4 import CalcIpv4
+import os
+
+if os.name == 'posix':
+    cls = "clear"
+else:
+    cls = "cls"
 
 """ calculo ipv4, com bbase em um ip  e uma mascara ou prefixo"""
 
-calc_ipv4 = CalcIpv4(ip='192.168.0.1' , prefixo=24)
+os.system(cls)
+
+ip = input('digite o ip: ')
+
+os.system(cls)
+
+
+
+maskOrPrefix = input(' digite a mascara ou o prefixo: ')
+
+os.system(cls)
+
+if len(maskOrPrefix) == 2:
+  calc_ipv4 = CalcIpv4(ip, prefixo=int(maskOrPrefix))
+else:
+  calc_ipv4 = CalcIpv4(ip, mascara=maskOrPrefix)
+
