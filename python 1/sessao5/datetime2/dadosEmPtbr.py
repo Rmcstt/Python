0d1a39519d2dbd91ccf99dbf0677cd06dfc9508a
rmcstt <anisio.nanato@hotmail.com>
2022-09-21 09:32:20 -0300

    3.0

--- python 1/sessao5/datetime2/dadosEmPtbr.py
+++ python 1/sessao5/datetime2/dadosEmPtbr.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 from locale import setlocale, LC_ALL
-from calendar import mdays
+from calendar import mdays, monthrange
+
+# monthrange(ano, mes) retorna uma tupla com o dia da semana e o numero de dias do mes
+
+# setlocale deffine a localidade
+
+# LC_ALL define a localidade para todos os tipos de dados
+
 
 setlocale(LC_ALL, 'pt_BR.utf-8')
 
@@ -11,3 +18,11 @@ formatacao2 = dt.strftime('%d/%m/%Y %H:%M:%S')
 print(formatacao)
 print(formatacao2)
 print(f'mes = {mes_atual}, ultimo dia do mes = {mdays[mes_atual]}')
+print(monthrange(2020,2))
+dia_semana, ultimo_dia = monthrange(2020,2)
+print(ultimo_dia)  # 29
+
+ultimos_dias_de_meses_do_ano_atual =  [
+  monthrange(datetime.now().year,mes)[1] for mes in range(1,13)
+]
+print(ultimos_dias_de_meses_do_ano_atual)
