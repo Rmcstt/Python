54d6084b5b3bd483c27db39c0e723e823a5b442f
rmcstt <anisio.nanato@hotmail.com>
2022-09-23 08:34:30 -0300

    1

--- python 1/sessao5/system/main.py
+++ python 1/sessao5/system/main.py
@@ -16,6 +16,25 @@ def formata_tamanho(tamanho):
   tera = base ** 4
   peta = base ** 5
 
+  if tamanho < kilo:
+    tamanho = base
+    texto = 'B'
+  elif tamanho < mega:
+    tamanho /= kilo
+    texto = 'K'
+  elif tamanho < giga:
+    tamanho /= mega
+    texto = 'M'
+  elif tamanho < tera:
+    tamanho /= giga
+    texto = 'G'
+  elif tamanho < peta:
+    tamanho /= tera
+    texto = 'T'
+
+  tamanho = round(tamanho, 2)
+  return f'{tamanho} {texto}'
+
 conta = 0
 for raiz, diretorios, arquivos in os.walk(caminho_procura):
   for arquivo in arquivos:
