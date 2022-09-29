6ec65fef61cdea22c113c317385f9002407cd804
rmcstt <anisio.nanato@hotmail.com>
2022-09-29 08:58:27 -0300

    2.0

--- python 1/sessao5/templateHTML/temp.py
+++ python 1/sessao5/templateHTML/temp.py
@@ -1,28 +1,29 @@
-from string import Template
-from datetime import datetime
-import random
+
+
+import smtplib
+import email.message
+
+def enviar_email():  
+    corpo_email = """
+    <p>bom diaaa renato</p>
+    <p>como voce esta?</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Assunto"
+    msg['From'] = 'renato.mota.costa@gmail.com'
+    msg['To'] = 'renato.mota.costa@gmail.com'
+    password = '' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
 
 
 
-lista = ['joao','maria', 'pedro', 'jose', 'marcos', 'ana', 'carlos', 'julia', 'josefa', 'josefina']
-
-sorteio = random.choice(lista)
-
-
-
-
-
-
-
-
-
-
-#----------------------------------------------#
-
-with open('/Users/renatomota/Desktop/Python/python 1/sessao5/templateHTML/template.html', 'r') as html:
-  template = Template(html.read())
-  data_atual = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-  corpo_msg = template.safe_substitute(nome=sorteio,data=data_atual)
-
-print(corpo_msg)
-print(sorteio)
+enviar_email()
