48b22eb6a7cec7d60b0079444a10b498059dbcf3
rmcstt <anisio.nanato@hotmail.com>
2022-10-05 09:00:04 -0300

    finally

--- python 1/sessao5/selenium/app2.py
+++ python 1/sessao5/selenium/app2.py
@@ -11,14 +11,33 @@ from time import sleep
 driver = webdriver.Safari()
 
 # Navegando para uma página
-driver.get('https://github.com')
+def site(url):
+    return driver.get('https://'+url)
 
+
+site('github.com')
+user = ('coloque seu usuário aqui')
+password = ('coloque sua senha aqui')
 sleep(1)
 
 driver.find_element(
     By.XPATH, '/html/body/div[1]/header/div/div[1]/div[1]/a').send_keys(Keys.ENTER)
 sleep(1)
-driver.find_element(
-    By.CSS_SELECTOR, 'body > div.position-relative.js-header-wrapper > header > div > div > div > a').send_keys(Keys.ENTER)
+
+driver.find_element(By.CSS_SELECTOR, 'body > div.position-relative.js-header-wrapper > header > div > div > div > a').send_keys(Keys.ENTER)
 sleep(1)
+driver.find_element(By.CSS_SELECTOR, '#login_field').send_keys(user)
+sleep(1)
+driver.find_element(By.CSS_SELECTOR, '#password').send_keys(password)
+sleep(2)
+driver.find_element(By.XPATH, '//*[@id="login"]/div[4]/form/div/input[11]').send_keys(Keys.ENTER)
+sleep(2)
+driver.find_element(By.CSS_SELECTOR, 'body > div.position-relative.js-header-wrapper > header > div:nth-child(7) > details > summary').send_keys(Keys.ENTER)
+sleep(2)
+driver.find_element(By.CSS_SELECTOR, 'body > div.position-relative.js-header-wrapper > header > div:nth-child(7) > details > details-menu > form > button').send_keys(Keys.ENTER)
+sleep(2)
+
 driver.quit()
+# driver.back()
+sleep(2)
+# driver.quit()
