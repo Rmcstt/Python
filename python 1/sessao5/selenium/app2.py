fbb5a1b360de11d055a986fe387d90f4c5b4d5c2
rmcstt <anisio.nanato@hotmail.com>
2022-10-04 23:56:48 -0300

    1

--- python 1/sessao5/selenium/app2.py
+++ python 1/sessao5/selenium/app2.py
@@ -1,79 +1,24 @@
+"""
+webdriver para Safari
+"""
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from time import sleep
 
+# Abrindo o navegador
+driver = webdriver.Safari()
 
-class ChromeAuto:
-    def __init__(self):
-        self.driver_path = '/Users/renatomota/Desktop/Python/python 1/sessao5/selenium/chromedriver'
-        self.options = webdriver.ChromeOptions()
-        self.options.add_argument('user-data-dir=Perfil')
-        self.chrome = webdriver.Chrome(
-            self.driver_path,
-            options=self.options
-        )
+# Navegando para uma página
+driver.get('https://github.com')
 
-    def clica_sign_in(self):
-        try:
-            btn_sign_in = self.chrome.find_element_by_link_text('Sign in')
-            btn_sign_in.click()
-        except Exception as e:
-            print('Erro ao clicar em Sign in:', e)
+sleep(1)
 
-    def acessa(self, site):
-        self.chrome.get(site)
-
-    def sair(self):
-        self.chrome.quit()
-
-    def clica_perfil(self):
-        try:
-            perfil = self.chrome.find_element_by_css_selector(
-                'body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-lg-flex > details')
-            perfil.click()
-        except Exception as e:
-            print('Erro ao clicar no perfil:', e)
-
-    def faz_logout(self):
-        try:
-            perfil = self.chrome.find_element_by_css_selector(
-                'body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-lg-flex > details > details-menu > form > button')
-            perfil.click()
-        except Exception as e:
-            print('Erro fazer logout:', e)
-
-    def verifica_usuario(self, usuario):
-        profile_link = self.chrome.find_element_by_class_name(
-            'user-profile-link')
-        profile_link_html = profile_link.get_attribute('innerHTML')
-        assert usuario in profile_link_html
-
-    def faz_login(self):
-        try:
-            input_login = self.chrome.find_element_by_id('login_field')
-            input_password = self.chrome.find_element_by_id('password')
-            btn_login = self.chrome.find_element_by_name('commit')
-
-            input_login.send_keys('')
-            input_password.send_keys('')
-            sleep(3)
-            btn_login.click()
-
-        except Exception as e:
-            print('Erro ao fazer login:', e)
-
-
-if __name__ == '__main__':
-    chrome = ChromeAuto()
-    chrome.acessa('https://youtube.com')
-
-    chrome.clica_perfil()
-    chrome.faz_logout()
-
-    chrome.clica_sign_in()
-    chrome.faz_login()
-
-    chrome.clica_perfil()
-    chrome.verifica_usuario('otaviomirandabr')
-
-    sleep(5)
-    chrome.sair()
+driver.find_element(
+    By.XPATH, '/html/body/div[1]/header/div/div[1]/div[1]/a').send_keys(Keys.ENTER)
+sleep(1)
+driver.find_element(
+    By.CSS_SELECTOR, 'body > div.position-relative.js-header-wrapper > header > div > div > div > a').send_keys(Keys.ENTER)
+sleep(1)
+driver.quit()
