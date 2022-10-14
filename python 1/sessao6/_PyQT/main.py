eaec1c885ea719681746af289d3f3dd64dcbc9af
rmcstt <anisio.nanato@hotmail.com>
2022-10-14 08:33:08 -0300

    2

--- python 1/sessao6/_PyQT/main.py
+++ python 1/sessao6/_PyQT/main.py
@@ -13,15 +13,20 @@ class App(QMainWindow):
     self.cw = QWidget()
     self.grid = QGridLayout(self.cw)
 
-    self.btn = QPushButton('botao')
+    self.btn = QPushButton('☠︎')
+    self.btn.setStyleSheet('font-size: 80px; color: red;')
     self.grid.addWidget(self.btn, 0,0, 1,1)
 
+    self.btn.clicked.connect(self.acao)
+
     self.setCentralWidget(self.cw)
+
+  def acao(self):
+    print('lascou')
 
 
 if __name__ == '__main__':
   qt = QApplication(sys.argv)
   app = App()
-  q = print('hello')
   app.show()
   qt.exec()
