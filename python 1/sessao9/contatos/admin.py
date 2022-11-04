a80bb70ac2d856cf6a5e4baaf33c4af9ea47755b
rmcstt <anisio.nanato@hotmail.com>
2022-11-04 08:05:40 -0300

    today

--- python 1/sessao9/contatos/admin.py
+++ python 1/sessao9/contatos/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Categoria, Contato
 
 # Register your models here.
+class ContatoAdmin(admin.ModelAdmin):
+  list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+  list_display_links = ('id', 'nome', 'sobrenome')
+  search_fields = ('nome', 'sobrenome', 'telefone')
+  list_per_page = 10
 
 admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
