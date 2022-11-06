25930e2c1b4508f7db7fbbc473c1b3f308c2507e
rmcstt <anisio.nanato@hotmail.com>
2022-11-06 16:31:58 -0300

    agenda dinamica

--- python 1/sessao9/contatos/admin.py
+++ python 1/sessao9/contatos/admin.py
@@ -5,9 +5,9 @@ from .models import Categoria, Contato
 class ContatoAdmin(admin.ModelAdmin):
   list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
   list_display_links = ('id', 'nome', 'sobrenome')
-  list_filter = ('nome', 'sobrenome')
+  list_filter = ('categoria', 'sobrenome')
   search_fields = ('nome', 'sobrenome', 'telefone')
-  list_per_page = 10
+  list_per_page = 50
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
