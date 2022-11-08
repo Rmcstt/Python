1cfae930e6d49454aa47866818d03b918f147863
rmcstt <anisio.nanato@hotmail.com>
2022-11-08 09:18:20 -0300

    filter

--- python 1/sessao9/contatos/views.py
+++ python 1/sessao9/contatos/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-  contatos = Contato.objects.all()
+  contatos = Contato.objects.order_by('-id').filter(mostrar=True)
   paginator = Paginator(contatos, 20)
   page = request.GET.get('p')
   contatos = paginator.get_page(page)
@@ -15,6 +15,10 @@ def index(request):
   })
 def ver_contato(request, contato_id):
   contato = get_object_or_404(Contato,id=contato_id)
+
+  if not contato.mostrar:
+    raise Http404()
+
   return render(request, 'contatos/ver_contato.html',{
     'contato' : contato
   })
