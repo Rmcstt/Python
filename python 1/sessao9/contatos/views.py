07f6b1eef25e6cfee00364bb2459c44ec0a0f9bf
rmcstt <anisio.nanato@hotmail.com>
2022-11-09 09:08:45 -0300

    busca termo

--- python 1/sessao9/contatos/views.py
+++ python 1/sessao9/contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 # Create your views here.
 
@@ -24,7 +26,16 @@ def ver_contato(request, contato_id):
   })
 
 def busca(request):
-  contatos = Contato.objects.order_by('-id').filter(mostrar=True)
+  termo = request.GET.get('termo')
+  if termo is None :
+    raise Http404()
+  campos = Concat('nome', Value(' '),'sobrenome')
+
+  contatos = Contato.objects.annotate(
+    nome_completo = campos
+  ).filter(
+    Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+  )
   paginator = Paginator(contatos, 20)
   page = request.GET.get('p')
   contatos = paginator.get_page(page)
