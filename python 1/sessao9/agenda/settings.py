f27d11331f4d226df381aaaecae1bb812c05f354
rmcstt <anisio.nanato@hotmail.com>
2022-11-10 09:32:20 -0300

    adiçao de foto para o contato

--- python 1/sessao9/agenda/settings.py
+++ python 1/sessao9/agenda/settings.py
@@ -124,3 +124,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'meida/'
