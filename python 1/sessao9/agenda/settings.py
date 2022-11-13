63e628f287048ce06f0bd17b8e2b3358c8d38514
rmcstt <anisio.nanato@hotmail.com>
2022-11-13 16:25:30 -0300

    campo de mensagem

--- python 1/sessao9/agenda/settings.py
+++ python 1/sessao9/agenda/settings.py
@@ -128,3 +128,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'meida/'
+
+# mensagens
+
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
--- python 1/sessao9/contatos/views.py
+++ python 1/sessao9/contatos/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
+  
   contatos = Contato.objects.order_by('-id').filter(mostrar=True)
   paginator = Paginator(contatos, 20)
   page = request.GET.get('p')
@@ -27,8 +29,9 @@ def ver_contato(request, contato_id):
 
 def busca(request):
   termo = request.GET.get('termo')
-  if termo is None :
-    raise Http404()
+  if termo is None or not termo :
+    messages.add_message(request, messages.ERROR, 'Campo termo não pode ficar vazio')
+    return redirect('index')
   campos = Concat('nome', Value(' '),'sobrenome')
 
   contatos = Contato.objects.annotate(
