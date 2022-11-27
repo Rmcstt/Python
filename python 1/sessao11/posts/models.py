317b8da6c6bb8fc9e2b3631fc734b7a131a71723
rmcstt <anisio.nanato@hotmail.com>
2022-11-27 17:38:48 -0300

    ha uma uz

--- python 1/sessao11/posts/models.py
+++ python 1/sessao11/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from categorias.models import Categoria
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+  id = models.AutoField(primary_key=True)
+  titulo_post = models.CharField(max_length=255)
+  autor_post = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+  data_post = models.DateTimeField(default=timezone.now)
+  conteudo_post = models.TextField()
+  excerto_post = models.TextField()
+  categoria_post = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, blank=True)
+  imagem_post = models.ImageField(upload_to='post_img/%Y/%m/%d',blank= True)
+  publicado_post = models.BooleanField(default=False)
