dec2072026f342071b5248f4f7456124312460a2
rmcstt <anisio.nanato@hotmail.com>
2022-12-02 09:44:57 -0300

    renderizando o blog

--- python 1/sessao11/blog/settings.py
+++ python 1/sessao11/blog/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -139,3 +139,6 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'alert-success',
     constants.INFO: 'alert-info',
     }
+
+INSTALLED_APPS += ['django_summernote',]
+X_FRAME_OPTIONS = 'SAMEORIGIN'
